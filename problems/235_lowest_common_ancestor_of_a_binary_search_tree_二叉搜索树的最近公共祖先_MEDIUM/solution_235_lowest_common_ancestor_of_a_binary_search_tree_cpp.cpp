55bafcec8fc99b8d5dae900dfc7510ef6c1c8818
silenceagle <czliuzechao@outlook.com>
2024-03-25 22:45:57 +0800

    feat: add problem 235, 236.

---
+++ problems/235_lowest_common_ancestor_of_a_binary_search_tree_二叉搜索树的最近公共祖先_MEDIUM/solution_235_lowest_common_ancestor_of_a_binary_search_tree_cpp.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root->val > p->val && root->val > q->val) {
+            return lowestCommonAncestor(root->left, p, q);
+        } else if (root->val < p->val && root->val < q->val) {
+            return lowestCommonAncestor(root->right, p, q);
+        } else {
+            return root;
+        }
+        
+    }
+};
+
+
+class Solution2 {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while (root) {
+            if (root->val > p->val && root->val > q->val) {
+                root = root->left;
+            } else if (root->val < p->val && root->val < q->val) {
+                root = root->right;
+            } else {
+                return root;
+            }
+        }
+        return root;
+        
+    }
+};
